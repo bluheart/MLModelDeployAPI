146517da2f3d9bac7d10f46c0785ee6f6c827941
bluheart <dreamnezumi@proton.me>
2025-11-13 13:44:26 -0300

    fixing

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM python:3.11
+
+LABEL authors="bluheart"
+
+RUN apt-get update && apt-get install -y curl build-essential
+
+ADD https://astral.sh/uv/install.sh /tmp/install_uv.sh
+
+RUN bash /tmp/install_uv.sh && rm /tmp/install_uv.sh
+
+# Ensure the installed binary is on the `PATH`
+ENV PATH="/root/.local/bin/:$PATH"
+
+WORKDIR /code
+
+COPY pyproject.toml uv.lock /code/
+
+# Install dependencies using UV based on your lock file
+RUN uv sync --locked --all-extras --no-dev
+
+ENV PATH="/code/.venv/bin:$PATH"
+
+COPY ./app /code/app
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app.server:app", "--host", "0.0.0.0", "--port", "8000"]
+
---
+++ app/server.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+model = joblib.load('app/model.joblib')
+
+class_names = np.array(['setosa', 'versicolor', 'virginica'])
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {'message': 'Iris model API'}
+
+@app.post("/predict")
+def predict(data: dict):
+    """
+    Predicts the class of a given set of features.
+    :param data:
+        data (dict): A dictionary containing features
+        e.g. {"features": [1, 2, 3, 4]}
+    :return:
+        dict: A dictionary containing the predicted class
+    """
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    class_name = class_names[prediction][0]
+    return {'prediction_class': class_name}
